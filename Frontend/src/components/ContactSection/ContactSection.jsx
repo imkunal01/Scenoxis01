34fbeabddcc4ec5154203db21a982a3eeed31f07
imkunal01 <142741387+imkunal01@users.noreply.github.com>
2025-11-11 22:30:42 +0530

    aksdbakbds

--- Frontend/src/components/ContactSection/ContactSection.jsx
+++ Frontend/src/components/ContactSection/ContactSection.jsx
@@ -31,7 +31,7 @@ const ContactSection = () => {
     setIsSubmitting(true);
     
     try {
-      const res = await fetch( 'http://localhost:5000/api/contact', {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/contact` || 'http://localhost:5000/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
